adf721a079bb3f37576e47280eb7ae82a6567d7b
Andinus <andinus@nand.sh>
2020-04-08 01:31:36 +0530

    Add wrapper around unix.UnveilBlock

---
+++ block.go
@@ -0,0 +1,9 @@
+package lynx
+
+import "golang.org/x/sys/unix"
+
+// UnveilBlock is just a wrapper around unix.UnveilBlock, it does
+// nothing extra. You should use unix.UnveilBlock.
+func UnveilBlock() error {
+	return unix.UnveilBlock()
+}
