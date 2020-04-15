bc9703246e571c39f9cdb2739b7a4b54ebf4bad2
Andinus <andinus@nand.sh>
2020-04-15 19:26:10 +0530

    Add Unveil & UnveilStrict func to unveil_other.go

--- unveil_other.go
+++ unveil_other.go
@@ -2,14 +2,28 @@
 
 package lynx
 
-// UnveilPath takes a path, permission & unveils it, it does nothing
+// Unveil takes a path, permission & unveils it, it does nothing
 // on non OpenBSD systems.
+func Unveil(_ string, _ string) error {
+	return nil
+}
+
+// UnveilPath takes a path, permission & unveils it, it does nothing
+// on non OpenBSD systems. Unveil should be used instead of
+// UnveilPath.
 func UnveilPath(_ string, _ string) error {
 	return nil
 }
 
-// UnveilPathStrict is just a wrapper around unix.Unveil. It does
+// UnveilStrict is just a wrapper around unix.Unveil. It does
 // nothing on non OpenBSD systems.
+func UnveilStrict(_ string, _ string) error {
+	return nil
+}
+
+// UnveilPathStrict is just a wrapper around unix.Unveil. It does
+// nothing on non OpenBSD systems. UnveilStrict should be used instead
+// of UnveilPathStrict.
 func UnveilPathStrict(_ string, _ string) error {
 	return nil
 }
